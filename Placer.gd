e911e24867c6a921706c323d3e18364d458ccd49
eliasjohansson <eljoh633@student.liu.se>
2023-02-03 20:42:39 +0100

    working on placer

--- Placer.gd
+++ Placer.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-
+var cells_in_inv : int = 0
 onready var grid = get_parent().get_node("Grid")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	# wait for player input to place cells
 	if Input.is_action_just_pressed("place"):
-		pass
+		var mouse_pos = get_global_mouse_position()
+		var did_place : bool= grid.place_cell(mouse_pos)
+		
+		# Only remove from inv if place was succesful
+		if did_place:
+			cells_in_inv -= 1
