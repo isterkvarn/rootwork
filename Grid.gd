020c5f5b753e935c411574a7e2cd89aadc683491
Dethcrvsh <fabianfabbe@hotmail.se>
2023-02-03 22:59:06 +0100

    stuff

--- Grid.gd
+++ Grid.gd
@@ -4,66 +4,89 @@ const BACKGROUND : int = 0
 const ROOT : int = 1
 const PISS : int = 2
 
-class SpawnRule:
-	var root_num : int # The number of roots needed in the vicinity
-	var root_range : int # The range which the roots must be within
-	
-	func _init(root_num_in : int, root_range_in):
-		root_num = root_num_in
-		root_range = root_range_in
-
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	do_step()
+	rng.randomize()
 
 # Do a time step
-func do_step():
-	print("do step")
+func do_step():	
+	var spawns = []
 	
-	set_cell(-1, -1, 2)
-	typeof(get_used_cells()[0])
-	var rule = SpawnRule.new(1, 1)
-	#print(get_root_neighbours(Vector2(0, 0)))
+	for root in get_used_cells():
+		if get_num_of_neighbours(root) > 3:
+			continue
+		
+		var cluster = get_spawn_coord(root)
+		var spawn_range = [[-1, 1], [-1, 1]]
+		
+		if cluster.x > 9:
+			spawn_range[0][1] = 0
+		elif cluster.x < -9:
+			spawn_range[0][0] = 0
+		if cluster.y > 9:
+			spawn_range[1][1] = 0
+		elif cluster.y > -9:
+			spawn_range[1][0] = 0
+		
+		var neighbour = Vector2(
+			rng.randi_range(spawn_range[0][0], spawn_range[0][1]) + root.x,
+			rng.randi_range(spawn_range[1][0], spawn_range[1][1]) + root.y
+		)
+			
+		
+		spawns.append(neighbour)
+	
+	for root in spawns:
+		place_root(root)
 
 func place_root(coord : Vector2):
-<<<<<<< HEAD
-	set_cell(coord.x, coord.y, ROOT)
-	
-# Determine if a root should be spawned on the coordinate
-func should_spawn(coord : Vector2) -> bool:
-	print(get_root_neighbours(Vector2(1, 1)))
-	return true
-=======
-	if get_cell(coord.x, coord.y):
+	if get_cell(coord.x, coord.y) != ROOT:
 		set_cell(coord.x, coord.y, ROOT)
 		return true;
 	return false;
->>>>>>> 9f805884eaefd6e3f5f8bcda58803e6de94d6572
 
-# Get the number of root neighbours
-func get_root_neighbours(root : Vector2) -> int:
+func get_random_neighbour(root : Vector2) -> Vector2:
+	var i : int = rng.randi_range(0, 8)
+	return Vector2(i/3 + root.x - 1, i%3 + root.y - 1)
+
+func get_num_of_neighbours(root : Vector2) -> int:
 	var num : int = 0
 	
 	for i in range(9):
 		if i == 4:
 			continue
-<<<<<<< HEAD
-			
-		var neighbour = Vector2(i / 3 - 1, i % 3 - 1)
-	
+		var neighbour = Vector2(i/3 + root.x - 1, i%3 + root.y - 1)
+		
 		if get_cell(neighbour.x, neighbour.y) == ROOT:
 			num += 1
 		
 	return num
-		
-		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-=======
-		neighbours.append(Vector2(i / 3 - 1, i % 3 - 1))
-	return neighbours
->>>>>>> 9f805884eaefd6e3f5f8bcda58803e6de94d6572
+func get_spawn_coord(root : Vector2) -> Vector2:
+	var cluster = Vector2(0, 0)
+	var root_range = 3
+	
+	var neighbours = pow((root_range * 2 + 1), 2)
+	
+	for i in range(neighbours):
+		var neighbour = Vector2(
+			i/(root_range*2 + 1) + root.x - root_range,
+			i%(root_range*2 + 1) + root.y - root_range
+		)
+		
+		if get_cell(neighbour.x, neighbour.y) != ROOT:
+			continue
+		
+		if neighbour.x < root.x:
+			cluster.x += -1
+		elif neighbour.x > root.x:
+			cluster.x += 1
+			
+		if neighbour.y < root.y:
+			cluster.y += -1
+		elif neighbour.y > root.y:
+			cluster.y += 1
+	
+	return cluster
